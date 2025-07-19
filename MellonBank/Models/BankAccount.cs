cfc7a177482eb8b9a77e062426129bce3d113909
Nikolaos Mallidis <nmallidis@gmail.com>
2025-07-19 12:38:29 +0300

    test commit

--- MellonBank/Models/BankAccount.cs
+++ MellonBank/Models/BankAccount.cs
@@ -5,26 +5,19 @@ namespace MellonBank.Models
 {
     public class BankAccount
     {
-        [Required]
         public int Id { get; set; }
 
-        [Required]
         public string UserAFM { get; set; }
 
-        [Required]
         public User User { get; set; }
 
-        [Required]
         public decimal BalanceEuro { get; set; }
 
-        [Required]
         [Column(TypeName = "decimal(18,2)")]
         public string AccountNumber { get; set; }
 
-        [Required]
         public string Branch { get; set; }
 
-        [Required]
         public string AccountType { get; set; }
     }
 }
